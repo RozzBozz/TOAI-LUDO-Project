33423391378f60d2a74a53782094785a70b112d9
RozzBozz <32929105+RozzBozz@users.noreply.github.com>
2022-05-26 00:05:25 +0200

    Added finishing touches before hand-in

--- AITestFramework.py
+++ AITestFramework.py
@@ -11,114 +11,118 @@ from ludoHelperFunctions import *
 # Which player number is the AI (If number 4 is chosen, it matches with the game video produced, if that is enabled)
 playerNumber = 1
 # How many games should be played for each test?
-numberOfGames = 3000
+numberOfGames = 1000
 # How many opponents?
 numberOfOpponents = 3
 # Are the enemies random players or semi smart players?
 randomEnemies = True # (NOT IMPLEMENTED)
 # Should the AI start from scratch, or just exploit the current knowledge (Q-table)?
-shouldLearn = True
+shouldLearn = False
 
 # Values to be tested
 epsilon = 1
-epsilonDecays = [0.1] # [%]
+epsilonDecays = [0.02] # [%]
 alphas = [0.5] # 
-gammas = [0.8] # 
+gammas = [0.4] # 
 
 for epsilonDecay in epsilonDecays:
     for alpha in alphas:
         for gamma in gammas:
-            # Initialization of AI
-            QTableFileName = "QTables/epsilonDecay{}alpha{}gamma{}.npy".format(epsilonDecay,alpha,gamma)
-            dataFileName = "dataFiles/epsilonDecay{}alpha{}gamma{}.txt".format(epsilonDecay,alpha,gamma)
-            winRatesFileName = "winRates/epsilonDecay{}alpha{}gamma{}.txt".format(epsilonDecay,alpha,gamma)
-            deltaQsFileName = "deltaQs/epsilonDecay{}alpha{}gamma{}.txt".format(epsilonDecay,alpha,gamma)
-            ludoAI = AI(QTableFileName, alpha, gamma, epsilon, epsilonDecay)
-
-            for gameNumber in range(1,numberOfGames+1):
-                # Print every 100th game
-                if gameNumber % 100 == 0:
-                    print("---------- Currently playing game number", gameNumber, "of", numberOfGames, "----------")
-                # Initialize game
-                if numberOfOpponents == 3:
-                    game = ludopy.Game()
+            for i in range(0,10):
+                # Initialization of AI
+                if shouldLearn:
+                    QTableFileName = "QTables/epsilonDecay{}alpha{}gamma{}.npy".format(epsilonDecay,alpha,gamma)
                 else:
-                    ghostPlayers = [1,2,3,4]
-                    ghostPlayers = np.delete(ghostPlayers,playerNumber)
-                    for i in range(0,numberOfOpponents-1):
-                        ghostPlayers = np.delete(ghostPlayers,i)
-                    game = ludopy.Game(ghostPlayers)
-                winnerFound = False
-                # Stores the index of the previous player
-                prevPlayer = None
-                # Stores the round number of the current game
-                round = 0
-                # Reset the state of the AI
-                if shouldLearn:
-                    ludoAI.reset()
-                # Specifies, that a state update, reward calculation and Q-table update shouldn't be done in the first round
-                doSRupdate = False
+                    QTableFileName = "QTable1Player.npy"
+                dataFileName = "dataFiles/epsilonDecay{}alpha{}gamma{}.txt".format(epsilonDecay,alpha,gamma)
+                winRatesFileName = "winRates/epsilonDecay{}alpha{}gamma{}.txt".format(epsilonDecay,alpha,gamma)
+                deltaQsFileName = "deltaQs/epsilonDecay{}alpha{}gamma{}.txt".format(epsilonDecay,alpha,gamma)
+                ludoAI = AI(QTableFileName, alpha, gamma, epsilon, epsilonDecay)
 
-                while not winnerFound:
-                    # Observe the game state. A get_obersvation call must be answered be a answer_observation call before it can be called again
-                    # diceRoll (int): Dice roll for the current player
-                    # movePieces (list of up to four ints): Index of the pieces that can be moved. E.g piece 0, 1, 2 or 3 (They are zero-indexed)
-                    # playerPieces (list of four ints): Indecies of the current players pieces on the board. Corresponds to the numeration of the game board
-                    # enemyPieces: (list of four ints): Indecies of the enemy pieces on the board. The indecies are given in teh players respective frames, NOT the current player
-                    # playerWinner (bool): True, if the current player is the winner
-                    # winnerFound (bool): True, if a winner has been found
-                    # curPlayer (int): Index of the current player
-                    (diceRoll, movePieces, playerPieces, enemyPieces, playerWinner, winnerFound), curPlayer = game.get_observation()
-                    allPieces = enemyPieces
-                    allPieces = np.append(enemyPieces, [playerPieces], axis=0)
-
-                    # The first player (index 0) starts a round
-                    # If it is the first round, account for the fact, that a player may cast the dice multiple times in a row during start, or if he/she hits a six
-                    if curPlayer == 0 and curPlayer != prevPlayer:
-                        round = round + 1
-                    prevPlayer = curPlayer
-
-                    # Input is allowed if it is the human/AI players turn
-                    if curPlayer == playerNumber-1:
-                        # Specifies which piece is moved. If no pieces can be moved, it is set to -1
-                        # If one or more pieces can be moved, this is set to the chosen piece, correpsonding to the index in the movePieces list
-                        pieceToMoveIndex = ludoAI.onePass(diceRoll,playerPieces,enemyPieces,shouldLearn)
-                        
-                        #img = ludopy.visualizer.make_img_of_board(allPieces, diceRoll, 3, round)
-                        #img = cv2.resize(img, (1088,900))
-                        #cv2.imshow("Current state of the board", img)
-                        #key = cv2.waitKey(0)
-                        #cv2.destroyWindow("Current state of the board")
+                for gameNumber in range(1,numberOfGames+1):
+                    # Print every 100th game
+                    if gameNumber % 100 == 0:
+                        print("---------- Currently playing game number", gameNumber, "of", numberOfGames, "----------")
+                    # Initialize game
+                    if numberOfOpponents == 3:
+                        game = ludopy.Game()
                     else:
-                        if len(movePieces):
-                            if randomEnemies:
-                                pieceToMoveIndex = movePieces[np.random.randint(0, len(movePieces))]
-                            else:
-                                pass
+                        ghostPlayers = [1,2,3,4]
+                        ghostPlayers = np.delete(ghostPlayers,playerNumber)
+                        for i in range(0,numberOfOpponents-1):
+                            ghostPlayers = np.delete(ghostPlayers,i)
+                        game = ludopy.Game(ghostPlayers)
+                    winnerFound = False
+                    # Stores the index of the previous player
+                    prevPlayer = None
+                    # Stores the round number of the current game
+                    round = 0
+                    # Reset the state of the AI
+                    if shouldLearn:
+                        ludoAI.reset()
+                    # Specifies, that a state update, reward calculation and Q-table update shouldn't be done in the first round
+                    doSRupdate = False
+
+                    while not winnerFound:
+                        # Observe the game state. A get_obersvation call must be answered be a answer_observation call before it can be called again
+                        # diceRoll (int): Dice roll for the current player
+                        # movePieces (list of up to four ints): Index of the pieces that can be moved. E.g piece 0, 1, 2 or 3 (They are zero-indexed)
+                        # playerPieces (list of four ints): Indecies of the current players pieces on the board. Corresponds to the numeration of the game board
+                        # enemyPieces: (list of four ints): Indecies of the enemy pieces on the board. The indecies are given in teh players respective frames, NOT the current player
+                        # playerWinner (bool): True, if the current player is the winner
+                        # winnerFound (bool): True, if a winner has been found
+                        # curPlayer (int): Index of the current player
+                        (diceRoll, movePieces, playerPieces, enemyPieces, playerWinner, winnerFound), curPlayer = game.get_observation()
+                        allPieces = enemyPieces
+                        allPieces = np.append(enemyPieces, [playerPieces], axis=0)
+
+                        # The first player (index 0) starts a round
+                        # If it is the first round, account for the fact, that a player may cast the dice multiple times in a row during start, or if he/she hits a six
+                        if curPlayer == 0 and curPlayer != prevPlayer:
+                            round = round + 1
+                        prevPlayer = curPlayer
+
+                        # Input is allowed if it is the human/AI players turn
+                        if curPlayer == playerNumber-1:
+                            # Specifies which piece is moved. If no pieces can be moved, it is set to -1
+                            # If one or more pieces can be moved, this is set to the chosen piece, correpsonding to the index in the movePieces list
+                            pieceToMoveIndex = ludoAI.onePass(diceRoll,playerPieces,enemyPieces,shouldLearn)
+                            
+                            #img = ludopy.visualizer.make_img_of_board(allPieces, diceRoll, 3, round)
+                            #img = cv2.resize(img, (1088,900))
+                            #cv2.imshow("Current state of the board", img)
+                            #key = cv2.waitKey(0)
+                            #cv2.destroyWindow("Current state of the board")
                         else:
-                            pieceToMoveIndex = -1
+                            if len(movePieces):
+                                if randomEnemies:
+                                    pieceToMoveIndex = movePieces[np.random.randint(0, len(movePieces))]
+                                else:
+                                    pass
+                            else:
+                                pieceToMoveIndex = -1
 
-                    _, _, _, _, _, winnerFound = game.answer_observation(pieceToMoveIndex)
+                        _, _, _, _, _, winnerFound = game.answer_observation(pieceToMoveIndex)
 
-                    if winnerFound:
-                        winningPlayer = curPlayer
+                        if winnerFound:
+                            winningPlayer = curPlayer
 
-                #print("-------------------- GAME", gameNumber, "OF", numberOfGames, "FINISHED --------------------")
+                    #print("-------------------- GAME", gameNumber, "OF", numberOfGames, "FINISHED --------------------")
 
-                ludoAI.addGamePlayed()
-                if winningPlayer == playerNumber-1:
-                    ludoAI.addWin()
-                ludoAI.addCurWinRate()
-                ludoAI.addCurDeltaQSum()
-                # Decay the epsilon-value after each game 
+                    ludoAI.addGamePlayed()
+                    if winningPlayer == playerNumber-1:
+                        ludoAI.addWin()
+                    ludoAI.addCurWinRate()
+                    ludoAI.addCurDeltaQSum()
+                    # Decay the epsilon-value after each game 
+                    if shouldLearn:
+                        ludoAI.decayEpsilon(epsilonDecay)
+                    #print("Number of games played:", ludoAI.getNumberOfGamesPlayed())
+                    #print("Number of games won:", ludoAI.getNumberOfGamesWon())
+                    #print("Current AI win rate: ", ludoAI.getCurWinRate() * 100, "%")
+
+                ludoAI.saveQTable(QTableFileName)
+                ludoAI.saveDataFile(dataFileName)
+                ludoAI.saveWinRates(winRatesFileName)
                 if shouldLearn:
-                    ludoAI.decayEpsilon(epsilonDecay)
-                #print("Number of games played:", ludoAI.getNumberOfGamesPlayed())
-                #print("Number of games won:", ludoAI.getNumberOfGamesWon())
-                #print("Current AI win rate: ", ludoAI.getCurWinRate() * 100, "%")
-
-            ludoAI.saveQTable(QTableFileName)
-            ludoAI.saveDataFile(dataFileName)
-            ludoAI.saveWinRates(winRatesFileName)
-            if shouldLearn:
-                ludoAI.saveDeltaQs(deltaQsFileName)
+                    ludoAI.saveDeltaQs(deltaQsFileName)
--- Plotting.m
+++ Plotting.m
@@ -1,11 +1,113 @@
-filepath = 'deltaQs/epsilonDecay0alpha0.05gamma0.7.txt';
+%% Plotting Q-value for experiment
+clear
+close all
+clc
+filepath = 'dataAnalysis/deltaQFig.txt';
 myTable = readtable(filepath);
 data = myTable{1,:};
 figure(1)
 plot(1:size(myTable,2),data)
+xlabel('Episode number')
+ylabel('Sum of ∆Q(s,a)-values)')
 
-filepath = 'deltaQs/epsilonDecay0alpha0.5gamma0.9.txt';
-myTable2 = readtable(filepath);
+%% Plotting win rates for different learning rates / discount factors
+clear
+close all
+clc
+folder = 'dataAnalysis/HyperParameterTuning/';
+alphaValues = [0.1, 0.2, 0.3, 0.4, 0.5];
+gammaValues = [0.4, 0.5, 0.6, 0.7, 0.8];
+useAlpha = false;
+filepaths = [];
+legendTitles = [];
+if useAlpha == true
+    for i = 1:size(alphaValues,2)
+        filepaths{i} = strcat(folder,'epsilonDecay0.02alpha',num2str(alphaValues(i)),'gamma0.8.txt');
+    end
+else
+    for i = 1:size(gammaValues,2)
+        filepaths{i} = strcat(folder,'epsilonDecay0.02alpha0.25gamma',num2str(gammaValues(i)),'.txt');
+    end
+end
+myTable1 = readtable(filepaths{1});
+data1 = myTable1{1,:};
+myTable2 = readtable(filepaths{2});
 data2 = myTable2{1,:};
-figure(2)
-plot(1:size(myTable2,2),data2)
+myTable3 = readtable(filepaths{3});
+data3 = myTable3{1,:};
+myTable4 = readtable(filepaths{4});
+data4 = myTable4{1,:};
+myTable5 = readtable(filepaths{5});
+data5 = myTable5{1,:};
+winRates = [data1(end), data2(end), data3(end), data4(end), data5(end)];
+if useAlpha == true
+    for i = 1:size(alphaValues,2)
+        legendTitles{i} = strcat('α=', num2str(alphaValues(i)), ' WR:', num2str(winRates(i)), '%');
+    end
+else
+    for i = 1:size(gammaValues,2)
+        legendTitles{i} = strcat('γ=', num2str(gammaValues(i)), ' WR:', num2str(winRates(i)), '%');
+    end
+end
+figure(1)
+hold on
+plot(1:size(myTable1,2),data1)
+plot(1:size(myTable1,2),data2)
+plot(1:size(myTable1,2),data3)
+plot(1:size(myTable1,2),data4)
+plot(1:size(myTable1,2),data5)
+if useAlpha == true
+    title('AIs trained with different learning rates γ=0.8, ε=1')
+    legend(legendTitles{1},legendTitles{2},legendTitles{3},legendTitles{4},legendTitles{5})
+else
+    title('AIs trained with different discound factors. α=0.25, ε=1')
+    legend(legendTitles{1},legendTitles{2},legendTitles{3},legendTitles{4},legendTitles{5})
+end
+xlabel('Episode number')
+ylabel('Cumulative win rate [%]')
+hold off
+
+%% Plotting average win rates
+clear
+close all
+clc
+trainNumber = 1;
+playNumber = 1;
+filepathWin = strcat('dataAnalysis/winRateTesting/Train',num2str(trainNumber),'Play',num2str(playNumber),'WinRates.txt');
+myTable1 = readtable(filepathWin);
+cumulativeWinRates = myTable1{:,:};
+avgWinRate = mean(cumulativeWinRates);
+stdDev = std(avgWinRate);
+f = figure(1);
+hold on
+plot(1:size(avgWinRate,2),avgWinRate)
+hold off
+if playNumber == 1
+    text = ' one player,';
+else
+    text = ' three players,';
+end
+if trainNumber == 1
+    text2 = ' one player';
+else
+    text2 = ' three players';
+end
+title(strcat('Trained on ', text2, ' played versus ', text, ' WR:',num2str(avgWinRate(end)),'% ±',num2str(stdDev)))
+f.Position = [300,300,600,500];
+xlabel('Episode number')
+ylabel('Average cumulative win rate [%]')
+
+%% Test for even mean
+playOnOne = true;
+if playOnOne == true
+    filepath1 = 'dataAnalysis/winRateTesting/Train1Play1WinRates.txt';
+    filepath2 = 'dataAnalysis/winRateTesting/Train3Play1WinRates.txt';
+else
+    filepath1 = 'dataAnalysis/winRateTesting/Train1Play3WinRates.txt';
+    filepath2 = 'dataAnalysis/winRateTesting/Train3Play3WinRates.txt';
+end
+myTable1 = readtable(filepath1);
+data1 = myTable1{:,:};
+myTable2 = readtable(filepath2);
+data2 = myTable2{:,:};
+[h,p] = ttest2(data1(:,end),data2(:,end))
